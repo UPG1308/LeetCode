b674f2167f592721d141ab322fb9941dd7e7ec9a
UDAYKUMAR P G <pguday4@gmail.com>
2022-10-01 02:38:43 +0530

    Create 2196. Create Binary Tree From Descriptions.java

---
+++ 2196. Create Binary Tree From Descriptions.java
@@ -0,0 +1,44 @@
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> map = new HashMap<>();
+        Set<Integer> childrenSet = new HashSet<>();
+        
+        for(int[] arr : descriptions)
+        {
+            int parentInt = arr[0];
+            int childInt = arr[1];
+            int isLeft = arr[2];
+            
+            childrenSet.add(childInt);
+            
+            TreeNode parentNode = map.getOrDefault(parentInt, new TreeNode(parentInt));
+            
+            if(isLeft == 1)
+            {
+                parentNode.left = map.getOrDefault(childInt, new TreeNode(childInt));
+                
+                map.put(childInt, parentNode.left);
+            }
+            else
+            {
+                parentNode.right = map.getOrDefault(childInt, new TreeNode(childInt));
+                
+                map.put(childInt, parentNode.right);
+                
+            }
+            map.put(parentInt, parentNode);
+            
+        }
+        int root = -1;
+            
+            for(int[] arr : descriptions)
+            {
+                if(!childrenSet.contains(arr[0]))
+                {
+                    root = arr[0];
+                    break;
+                }
+            }
+        return map.getOrDefault(root, null);
+    }
+}
